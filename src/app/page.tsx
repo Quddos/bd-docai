c8f43d876b1af7704a6161dd014b6f1f6934294c
Raheem Qudus <84246065+Quddos@users.noreply.github.com>
2024-05-06 23:11:06 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   return (
-   <h1 className="text-red-600">Hello</h1>
+  
+    <Button>Click Me</Button>
   );
 }
